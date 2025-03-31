12c6802b04b970d30ead3d22a93d5ed8b0dae5a3
Minal Patil <patilminal322@gmail.com>
2025-03-31 19:44:21 +0530

    Practice Pytest for simple basic files , run , multiple tests

---
+++ Pytest practice/test_demo2.py
@@ -0,0 +1,9 @@
+
+def test_check_valid():
+    var = "Hi"
+    assert "Hi" == var, "Test failed because ,string did not match"
+
+def test_credit_card_valid():
+    num1 = 2
+    num2 = 3
+    assert num1+num2 == 5
---
+++ Pytest practice/test_hello.py
@@ -0,0 +1,15 @@
+
+def test_first_fun():
+    print("Hello")
+
+def test_first_fun2():
+    print("Hello")
+
+def test_first_fun3():
+    print("Hello")
+
+
+def test_credit_card_verify():
+    num1 = "hello"
+    num2 = "hello"
+    assert num1 == num2
